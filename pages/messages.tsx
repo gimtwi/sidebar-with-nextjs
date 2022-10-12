d300f7fb41d2a9834894bc6717ab86d9bcff1dd2
mgrauberger <mgrauberger@brews38.pribas>
2022-10-12 12:45:42 +0200

    feat: navigation sidebar

--- pages/messages.tsx
+++ pages/messages.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
 import NavBar from "../component/NavBar";
 
-const Home: NextPage = () => {
+const Messages: NextPage = () => {
   return (
     <div>
-      <NavBar home="active" />
+      <NavBar messages="active" />
       <main>
-        <h1>Home Page</h1>
+        <h1>Messages Page</h1>
       </main>
     </div>
   );
 };
 
-export default Home;
+export default Messages;
--- pages/user.tsx
+++ pages/user.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
 import NavBar from "../component/NavBar";
 
-const Home: NextPage = () => {
+const User: NextPage = () => {
   return (
     <div>
-      <NavBar home="active" />
+      <NavBar user="active" />
       <main>
-        <h1>Home Page</h1>
+        <h1>User Page</h1>
       </main>
     </div>
   );
 };
 
-export default Home;
+export default User;
